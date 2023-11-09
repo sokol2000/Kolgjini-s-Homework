93c56887e90a161a29631edced27a28410d569a8
sokol2000 <117646528+sokol2000@users.noreply.github.com>
2023-11-09 18:11:33 +0100

    Add files via upload

---
+++ HOMEWORK 5/C# PART/Attack.cs
@@ -0,0 +1,359 @@
+﻿using System.Diagnostics;
+
+namespace hom5
+{
+    internal class Attack
+    {
+        private Random rand;
+        private AttackForm atkForm;
+        private Stopwatch sw;
+
+        // Pens to draw all the machines
+        private Pen[] pens = {
+                new Pen(Color.Olive, 3),
+                new Pen(Color.Red, 3),
+                new Pen(Color.Blue, 3),
+                new Pen(Color.Green, 3),
+                new Pen(Color.Orange, 3),
+                new Pen(Color.Aqua, 3),
+                new Pen(Color.Coral, 3),
+                new Pen(Color.Purple, 3),
+                new Pen(Color.Salmon, 3),
+                new Pen(Color.SandyBrown, 3)
+        };
+
+        private Pen axis_pen;
+        private Pen company_pen;
+        private Pen rect1_pen;
+        private Brush histo_pen;
+
+        public Thread thread;
+        private int nAttacks;
+        public int picking;
+        
+
+        public int mSystems;
+        private double lambdaAttack;
+        public int aHistogram;
+
+        private List<List<Point>> zero_points;
+        private List<int> zero_points_Y = new List<int>();
+
+        private Rectangle zero_histo;
+        private Rectangle zero_histo_end;
+        private Dictionary<int, int> countZero_points;
+        private Dictionary<int, int> countZero_points_end;
+
+        private int barHeight = 10;
+        private int scaling = 5;
+
+        public Attack(AttackForm atkForm)
+        {
+            this.atkForm = atkForm;
+            this.rand = new Random();
+            this.sw = new Stopwatch();
+
+            this.histo_pen = new SolidBrush(Color.FromArgb(150, Color.Maroon));
+            this.axis_pen = new Pen(Color.DarkGray, 2);
+            this.company_pen = new Pen(Color.LightGray, 1);
+            this.rect1_pen = new Pen(Color.LightBlue, 4);
+            this.zero_points = new List<List<Point>>();
+
+            this.countZero_points = new Dictionary<int, int>();
+            this.countZero_points_end = new Dictionary<int, int>();
+            this.zero_histo = new Rectangle(0, 0, 0, this.barHeight);
+            this.zero_histo_end = new Rectangle(0, 0, 0, this.barHeight);
+
+            this.picking = -1;
+            this.nAttacks = 1;
+            this.lambdaAttack = 0;
+            this.mSystems = 1;
+            this.aHistogram = 1;
+
+            this.thread = new Thread(new ThreadStart(StartThreadedAttacks));
+
+            // Starting thread
+            this.thread.Start();
+        }
+
+        public void startAttacks(int n, double lambda, int m, int a)
+        {
+            this.zero_points.Clear();
+            this.zero_points_Y.Clear();
+            this.countZero_points.Clear();
+            this.countZero_points_end.Clear();
+
+            for (int i = 0; i < m; i++)
+            {
+                this.zero_points.Add(new List<Point>());
+                this.zero_points[i].Add(new Point(0, 0));
+                this.zero_points_Y.Add(0);
+            }
+
+            this.lambdaAttack = lambda;
+            this.nAttacks = n;
+            this.mSystems = m;
+            this.picking = 0;
+            this.aHistogram = a;
+        }
+
+        public void cancelAttack()
+        {
+            if (this.picking >= this.aHistogram) this.picking = -3;
+            else this.picking = -4;
+
+            // Reset UI to default values (Thread  safe)
+            this.atkForm.BeginInvoke((Action)delegate ()
+            {
+                this.atkForm.cancBT.Enabled = false;
+                this.atkForm.startBT.Enabled = true;
+                this.atkForm.nValue.Enabled = true;
+                this.atkForm.lambdaValue.Enabled = true;
+            });
+
+        }
+
+        private void StartThreadedAttacks()
+        {
+            int posX = 0; int posY = 0, index = 0, max = 0;
+            int x = 0, y = 0, newZero_Y = 0;
+
+            double randNumber;
+            Point newZero;
+
+            while (this.atkForm.running)
+            {
+                sw.Start();
+
+                if (this.picking > -1 && this.picking < this.nAttacks)
+                {
+                    this.atkForm.BeginInvoke((Action)delegate ()
+                    {
+                        this.atkForm.nValue.Enabled = false;
+                        this.atkForm.lambdaValue.Enabled = false;
+                        this.atkForm.startBT.Enabled = false;
+                        this.atkForm.cancBT.Enabled = true;
+                    });
+
+                    if (this.picking == this.aHistogram)
+                    {
+                        max = 0;
+                        for (int i = 0; i < this.mSystems; i++) if (this.zero_points_Y[i] > max) max = this.zero_points_Y[i];
+                        for (int i = 0; i <= max; i++) countZero_points[i] = 0;
+                        for (int i = 0; i < this.mSystems; i++) countZero_points[this.zero_points_Y[i]] += 1;
+                    }
+
+                    for (int i = 0; i < this.mSystems; i++)
+                    {
+                        randNumber = rand.NextDouble();
+                        newZero = this.zero_points[i].Last();
+                        newZero_Y = this.zero_points_Y[i];
+
+                        newZero.X++;
+                        newZero.Y += (randNumber <= this.lambdaAttack/(this.nAttacks + 1) ? -1 : 0);
+                        newZero_Y += (randNumber <= this.lambdaAttack / (this.nAttacks + 1) ? 1 : 0);
+
+                        this.zero_points[i].Add(newZero);
+                        this.zero_points_Y[i] = newZero_Y;
+                    }
+
+                    this.picking += 1;
+                }
+
+                if(this.picking == this.nAttacks)
+                {
+                    max = 0;
+                    for (int i = 0; i < this.mSystems; i++) if (this.zero_points_Y[i] > max) max = this.zero_points_Y[i];
+                    for (int i = 0; i <= max; i++) countZero_points_end[i] = 0;
+                    for (int i = 0; i < this.mSystems; i++) countZero_points_end[this.zero_points_Y[i]] += 1;
+
+                    this.picking = -2;
+                }
+
+                if (this.picking == -2)
+                {
+                    // Reset UI to default values (Thread  safe)
+                    this.atkForm.BeginInvoke((Action)delegate ()
+                    {
+                        this.atkForm.cancBT.Enabled = false;
+                        this.atkForm.startBT.Enabled = true;
+                        this.atkForm.nValue.Enabled = true;
+                        this.atkForm.lambdaValue.Enabled = true;
+                    });
+                }
+
+                // Drawing all the things
+                this.atkForm.grph.Clear(Color.Transparent);
+                try
+                {
+                    this.atkForm.Invoke(new MethodInvoker(delegate ()
+                    {
+                        // Draw rectangle
+                        this.atkForm.grph.DrawRectangle(this.rect1_pen, this.atkForm.rect1);
+
+                        // Draw axis in rectangle
+                        // - Horizontal
+                        posY = this.atkForm.rect1.Y + (int)this.atkForm.rect1.Height / 2 + this.atkForm.r1move.Y;
+                        if (!(posY < this.atkForm.rect1.Top || posY > this.atkForm.rect1.Bottom)) this.atkForm.grph.DrawLine(this.axis_pen, this.atkForm.rect1.Left, posY, this.atkForm.rect1.Right, posY);
+
+                        // smaller lines
+                        index = 0;
+                        while (posY - index * this.atkForm.r1zoom > this.atkForm.rect1.Top)
+                        {
+                            if (posY - index * this.atkForm.r1zoom < this.atkForm.rect1.Bottom) this.atkForm.grph.DrawLine(this.company_pen, this.atkForm.rect1.Left, posY - index * this.atkForm.r1zoom, this.atkForm.rect1.Right, posY - index * this.atkForm.r1zoom);
+                            index += this.scaling;
+                        }
+                        index = 0;
+                        while (posY + index * this.atkForm.r1zoom < this.atkForm.rect1.Bottom)
+                        {
+                            if (posY + index * this.atkForm.r1zoom > this.atkForm.rect1.Top) this.atkForm.grph.DrawLine(this.company_pen, this.atkForm.rect1.Left, posY + index * this.atkForm.r1zoom, this.atkForm.rect1.Right, posY + index * this.atkForm.r1zoom);
+                            index += this.scaling;
+                        }
+
+                        // - Vertical
+                        posX = this.atkForm.rect1.X + this.atkForm.r1move.X + (int)this.atkForm.rect1.Width / 2;
+                        if (!(posX < this.atkForm.rect1.Left || posX > this.atkForm.rect1.Right)) this.atkForm.grph.DrawLine(this.axis_pen, this.atkForm.rect1.Left + this.atkForm.r1move.X + (int)this.atkForm.rect1.Width / 2, this.atkForm.rect1.Top, this.atkForm.rect1.Left + this.atkForm.r1move.X + (int)this.atkForm.rect1.Width / 2, this.atkForm.rect1.Bottom);
+
+                        // smaller lines
+                        index = 0;
+                        while (posX - index * this.atkForm.r1zoom > this.atkForm.rect1.Left)
+                        {
+                            if (posX - index * this.atkForm.r1zoom < this.atkForm.rect1.Right) this.atkForm.grph.DrawLine(this.company_pen, posX - index * this.atkForm.r1zoom, this.atkForm.rect1.Top, posX - index * this.atkForm.r1zoom, this.atkForm.rect1.Bottom);
+                            index += this.scaling;
+                        }
+                        index = 0;
+                        while (posX + index * this.atkForm.r1zoom < this.atkForm.rect1.Right)
+                        {
+                            if (posX + index * this.atkForm.r1zoom > this.atkForm.rect1.Left) this.atkForm.grph.DrawLine(this.company_pen, posX + index * this.atkForm.r1zoom, this.atkForm.rect1.Top, posX + index * this.atkForm.r1zoom, this.atkForm.rect1.Bottom);
+                            index += this.scaling;
+                        }
+
+                        // Drawing graphs in the rectangle
+                        for (int i = 0; i < this.zero_points.Count() && this.atkForm.running; i++)
+                        {
+                            for (int j = 0; j < this.zero_points[i].Count() - 1 && this.atkForm.running; j++)
+                            {
+                                Point a = this.zero_points[i].ElementAt(j);
+                                Point b = this.zero_points[i].ElementAt(j + 1);
+
+                                // Applying zoom effect
+                                a.X = (int)(a.X * this.atkForm.r1zoom * this.scaling);
+                                a.Y = (int)(a.Y * this.atkForm.r1zoom * this.scaling);
+                                b.X = (int)(b.X * this.atkForm.r1zoom * this.scaling);
+                                b.Y = (int)(b.Y * this.atkForm.r1zoom * this.scaling);
+
+                                // Moving points into rectangle
+                                a.X += this.atkForm.rect1.Left; a.Y += this.atkForm.rect1.Top;
+                                b.X += this.atkForm.rect1.Left; b.Y += this.atkForm.rect1.Top;
+
+                                // Starting from the middle of the rectangle
+                                a.X += (int)this.atkForm.rect1.Width / 2;
+                                a.Y += (int)this.atkForm.rect1.Height / 2;
+                                b.X += (int)this.atkForm.rect1.Width / 2;
+                                b.Y += (int)this.atkForm.rect1.Height / 2;
+
+                                // Moving based on the mov items
+                                a.X += this.atkForm.r1move.X;
+                                a.Y += this.atkForm.r1move.Y;
+                                b.X += this.atkForm.r1move.X;
+                                b.Y += this.atkForm.r1move.Y;
+
+                                // In case it's out of the rectangle, I can skip the draw
+                                if (b.X > this.atkForm.rect1.Right || b.X < this.atkForm.rect1.Left || b.Y > this.atkForm.rect1.Bottom || b.Y < this.atkForm.rect1.Top) continue;
+                                if (a.X > this.atkForm.rect1.Right || a.X < this.atkForm.rect1.Left || a.Y > this.atkForm.rect1.Bottom || a.Y < this.atkForm.rect1.Top) continue;
+
+                                this.atkForm.grph.DrawLine(this.pens[i % this.pens.Count()], a, b);
+                            }
+                        }
+
+                        if (this.picking == -2 || this.picking == -3 || this.picking > this.aHistogram)
+                        {
+                            foreach (var item in countZero_points)
+                            {
+                                if (item.Value == 0) continue;
+
+                                x = this.aHistogram;
+                                y = -(int)(item.Key);
+
+                                x = (int)(x * this.atkForm.r1zoom * this.scaling);
+                                y = (int)(y * this.atkForm.r1zoom * this.scaling);
+
+                                // Moving points into rectangle
+                                x += this.atkForm.rect1.Left; y += this.atkForm.rect1.Top;
+
+                                // Starting from the middle of the rectangle
+                                x += (int)this.atkForm.rect1.Width / 2;
+                                y += (int)this.atkForm.rect1.Height / 2;
+
+                                // Moving based on the mov items
+                                x += this.atkForm.r1move.X;
+                                y += this.atkForm.r1move.Y;
+
+
+                                this.zero_histo.X = x;
+                                this.zero_histo.Y = y - (int)(this.scaling * this.atkForm.r1zoom / 2);
+                                this.zero_histo.Width = Math.Max((int)(item.Value * this.atkForm.r1zoom * this.scaling), (1 + item.Value) * this.scaling);
+                                this.zero_histo.Height = (int)(this.scaling * this.atkForm.r1zoom);
+
+                                if (this.zero_histo.Right > this.atkForm.rect1.Right || this.zero_histo.Left < this.atkForm.rect1.Left || this.zero_histo.Bottom > this.atkForm.rect1.Bottom || this.zero_histo.Top < this.atkForm.rect1.Top) continue;
+
+                                this.atkForm.grph.FillRectangle(this.histo_pen, this.zero_histo);
+                            }
+                        }
+
+                        if (this.picking == -2)
+                        {
+                            foreach (var item in countZero_points_end)
+                            {
+                                if (item.Value == 0) continue;
+
+                                x = this.nAttacks;
+                                y = -(int)(item.Key);
+
+                                x = (int)(x * this.atkForm.r1zoom * this.scaling);
+                                y = (int)(y * this.atkForm.r1zoom * this.scaling);
+
+                                // Moving points into rectangle
+                                x += this.atkForm.rect1.Left; y += this.atkForm.rect1.Top;
+
+                                // Starting from the middle of the rectangle
+                                x += (int)this.atkForm.rect1.Width / 2;
+                                y += (int)this.atkForm.rect1.Height / 2;
+
+                                // Moving based on the mov items
+                                x += this.atkForm.r1move.X;
+                                y += this.atkForm.r1move.Y;
+
+                                this.zero_histo_end.X = x;
+                                this.zero_histo_end.Y = y - (int)(this.scaling * this.atkForm.r1zoom / 2);
+                                this.zero_histo_end.Width = (int) Math.Ceiling(item.Value * this.atkForm.r1zoom * this.scaling);
+                                this.zero_histo_end.Height = (int)(this.scaling * this.atkForm.r1zoom);
+
+                                if ((x + zero_histo_end.Width) > this.atkForm.rect1.Right || x < this.atkForm.rect1.Left || y > this.atkForm.rect1.Bottom || y < this.atkForm.rect1.Top) continue;
+
+
+                                this.atkForm.grph.FillRectangle(this.histo_pen, this.zero_histo_end);
+                            }
+                        }
+
+                        this.atkForm.picBox.Refresh();
+                    }));
+                }
+                catch (ObjectDisposedException ex) { break; }
+
+                sw.Stop();
+                // Max ~30fps (delta time calculation)
+                double sleeptime = 33.34 - sw.ElapsedMilliseconds;
+                if (sleeptime > 0) Thread.Sleep((int)sleeptime);
+                sw.Reset();
+            }
+
+            for (int i = 0; i < this.pens.Count(); i++) this.pens[i].Dispose();
+            this.axis_pen.Dispose();
+            this.company_pen.Dispose();
+            this.rect1_pen.Dispose();
+            this.histo_pen.Dispose();
+            return;
+        }
+    }
+}
---
+++ HOMEWORK 5/C# PART/AttackForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace hom5
+{
+    partial class AttackForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            nValue = new NumericUpDown();
+            cancBT = new Button();
+            startBT = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            bindingSource1 = new BindingSource(components);
+            picBox = new PictureBox();
+            label3 = new Label();
+            mSystems = new NumericUpDown();
+            label4 = new Label();
+            A = new NumericUpDown();
+            lambdaValue = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)nValue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mSystems).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)A).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)lambdaValue).BeginInit();
+            SuspendLayout();
+            // 
+            // nValue
+            // 
+            nValue.Location = new Point(163, 11);
+            nValue.Margin = new Padding(4, 2, 4, 2);
+            nValue.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
+            nValue.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nValue.Name = "nValue";
+            nValue.Size = new Size(119, 39);
+            nValue.TabIndex = 1;
+            nValue.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // cancBT
+            // 
+            cancBT.Location = new Point(1926, 15);
+            cancBT.Margin = new Padding(4, 2, 4, 2);
+            cancBT.Name = "cancBT";
+            cancBT.Size = new Size(123, 47);
+            cancBT.TabIndex = 3;
+            cancBT.Text = "Cancel";
+            cancBT.UseVisualStyleBackColor = true;
+            cancBT.Click += cancBT_Click;
+            // 
+            // startBT
+            // 
+            startBT.Location = new Point(1796, 13);
+            startBT.Margin = new Padding(4, 2, 4, 2);
+            startBT.Name = "startBT";
+            startBT.Size = new Size(123, 47);
+            startBT.TabIndex = 4;
+            startBT.Text = "Start";
+            startBT.UseVisualStyleBackColor = true;
+            startBT.Click += startBT_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 17);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 32);
+            label1.TabIndex = 6;
+            label1.Text = "N:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1161, 17);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 32);
+            label2.TabIndex = 7;
+            label2.Text = "Choose λ:";
+            // 
+            // picBox
+            // 
+            picBox.Location = new Point(15, 70);
+            picBox.Margin = new Padding(4, 2, 4, 2);
+            picBox.Name = "picBox";
+            picBox.Size = new Size(2043, 1067);
+            picBox.TabIndex = 8;
+            picBox.TabStop = false;
+            picBox.MouseDown += mouseDown;
+            picBox.MouseMove += mouseMove;
+            picBox.MouseUp += mouseUp;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(355, 17);
+            label3.Margin = new Padding(7, 0, 7, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 32);
+            label3.TabIndex = 9;
+            label3.Text = "M systems:";
+            // 
+            // mSystems
+            // 
+            mSystems.Location = new Point(494, 11);
+            mSystems.Margin = new Padding(7, 6, 7, 6);
+            mSystems.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            mSystems.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            mSystems.Name = "mSystems";
+            mSystems.Size = new Size(126, 39);
+            mSystems.TabIndex = 10;
+            mSystems.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(704, 21);
+            label4.Margin = new Padding(7, 0, 7, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(226, 32);
+            label4.TabIndex = 11;
+            label4.Text = "attack on histogram";
+            // 
+            // A
+            // 
+            A.Location = new Point(943, 15);
+            A.Margin = new Padding(7, 6, 7, 6);
+            A.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            A.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            A.Name = "A";
+            A.Size = new Size(126, 39);
+            A.TabIndex = 12;
+            A.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // lambdaValue
+            // 
+            lambdaValue.Location = new Point(1287, 17);
+            lambdaValue.Margin = new Padding(4, 2, 4, 2);
+            lambdaValue.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            lambdaValue.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            lambdaValue.Name = "lambdaValue";
+            lambdaValue.Size = new Size(141, 39);
+            lambdaValue.TabIndex = 2;
+            lambdaValue.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // AttackForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(2069, 1150);
+            Controls.Add(A);
+            Controls.Add(label4);
+            Controls.Add(mSystems);
+            Controls.Add(label3);
+            Controls.Add(picBox);
+            Controls.Add(lambdaValue);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(startBT);
+            Controls.Add(cancBT);
+            Controls.Add(nValue);
+            Margin = new Padding(4, 2, 4, 2);
+            Name = "AttackForm";
+            Text = "Form1";
+            FormClosing += closingFunc;
+            ResizeEnd += resizePicBox;
+            ((System.ComponentModel.ISupportInitialize)nValue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mSystems).EndInit();
+            ((System.ComponentModel.ISupportInitialize)A).EndInit();
+            ((System.ComponentModel.ISupportInitialize)lambdaValue).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        public NumericUpDown nValue;
+        public Button cancBT;
+        public Button startBT;
+
+        private Label label1;
+        private Label label2;
+        private BindingSource bindingSource1;
+        public PictureBox picBox;
+        private Label label3;
+        private NumericUpDown mSystems;
+        private Label label4;
+        private NumericUpDown A;
+        public NumericUpDown lambdaValue;
+    }
+}
